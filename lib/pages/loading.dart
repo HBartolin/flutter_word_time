68bf97493a9e47952842b5f5e2e5f5d7e1354c1a
Hrvoje Bartolin <hbartolin@gmail.com>
2023-10-04 13:17:30 +0200

    3

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -7,9 +9,19 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
   @override
+  void initState() {
+    getData();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Text("Loading"),
     );
+  }
+
+  void getData() async {
+    Response response= await get("https://jsonplaceholder.typicode.com/todos/1" as Uri);
+    Map data=jsonDecode(response.body);
   }
 }
